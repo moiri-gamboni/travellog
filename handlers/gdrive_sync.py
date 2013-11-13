373d573a00a8d89c6081b8f2ed7cd7d759f4d866
lingz <lz781@nyu.edu>
2013-11-13 04:07:54 +0400

    installed dependcies

--- handlers/gdrive_sync.py
+++ handlers/gdrive_sync.py
@@ -4,17 +4,16 @@ import pprint
 from api_keys import ACCESS_TOKEN, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, TOKEN_EXPIRY, TOKEN_URI
 from apiclient.discovery import build
 from oauth2client.client import OAuth2Credentials
-from apiclient.http import MediaFileUpload
 
 creds = OAuth2Credentials(ACCESS_TOKEN, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, TOKEN_EXPIRY, TOKEN_URI, None)
 http = creds.authorize(httplib2.Http())
 drive_service = build("drive", "v2", http=http)
-media_body = MediaFileUpload("model.py", mimetype='text/plain', resumable=True)
-body = {
-  'title': 'My document',
-  'description': 'A test document',
-  'mimeType': 'text/plain'
-}
+#media_body = MediaFileUpload("model.py", mimetype='text/plain', resumable=True)
+#body = {
+  #'title': 'My document',
+  #'description': 'A test document',
+  #'mimeType': 'text/plain'
+#}
 
-file = drive_service.files().insert(body=body, media_body=media_body).execute()
-pprint.pprint(file)
+#file = drive_service.files().insert(body=body, media_body=media_body).execute()
+#pprint.pprint(file)
