818519e422a46a369f60db4122aa048fe9ce11e2
Moiri Gamboni <Moiri Gamboni>
2013-11-20 04:24:18 +0400

    Service bugs on log gets

--- static/js/gservices.js
+++ static/js/gservices.js
@@ -5,7 +5,6 @@ var scopes = "https://www.googleapis.com/auth/plus.me"+
   " https://www.googleapis.com/auth/userinfo.profile";
 function handleClientLoad() {
   // Step 2: Reference the API key
-  console.log("This is working");
   gapi.client.setApiKey(apiKey);
   window.setTimeout(checkAuth,1);
 }
@@ -15,16 +14,13 @@ function checkAuth() {
 }
 
 function handleAuthResult(authResult) {
-  console.log(1)
   var successMessage = $("#loggedIn");
   var authorizeButton = $("#authorize-button");
   if (authResult && !authResult.error) {
-    console.log(2)
     authorizeButton.css({visibility:'hidden'});
     successMessage.css({visibility:'visible'});
     getUserInfo();
   } else {
-    console.log(3)
     successMessage.css({visibility:'hidden'});
     authorizeButton.css({visibility:'visible'}).on("click", handleAuthClick);
   }
